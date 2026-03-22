cf48de6045010081b1beef63c2fa99bb5f0a9194
msriravindranath <ravindranathmittamedi143@gmail.com>
2026-03-22 13:36:31 +0530

    Update and rename keys.py to keys_example.py

--- keys.py
+++
@@ -1,5 +0,0 @@
-
-accountSID = '********************************' # Your Twilio Account SID
-authToken = '********************************'# Your Twilio authentication Token
-twilioNumber = '*************'  # Your Twilio virtual number
-targetNumber = '*************' # Your personal phone number
---
+++ keys_example.py
@@ -0,0 +1,5 @@
+
+accountSID = 'your_twilio_account_SID_here' # Your Twilio Account SID
+authToken = 'your_twilio_authToken_here'# Your Twilio authentication Token
+twilioNumber = 'your_Twilio_number_here'  # Your Twilio virtual number
+targetNumber = 'Your_Target_Phone_number' # Your personal phone number
