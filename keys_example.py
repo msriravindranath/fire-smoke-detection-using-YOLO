14e028420afe0e8ada64baa7c5d012f265b4c27b
msriravindranath <ravindranathmittamedi143@gmail.com>
2026-07-17 19:53:53 +0530

    Refactor Twilio credentials and add test number
    Updated Twilio credentials and added test number.

--- keys_example.py
+++ keys_example.py
@@ -1,5 +1,11 @@
+# Twilio credentials
+accountSID = "your_twilio_account_sid"
+authToken = "your_twilio_auth_token"
+twilioNumber = "your_twilio_virtual_number"
 
-accountSID = 'your_twilio_account_SID_here' # Your Twilio Account SID
-authToken = 'your_twilio_authToken_here'# Your Twilio authentication Token
-twilioNumber = 'your_Twilio_number_here'  # Your Twilio virtual number
-targetNumber = 'Your_Target_Phone_number' # Your personal phone number
+# Property owner's phone number
+ownerNumber = "your_owner_test_number"
+
+# TEST NUMBER ONLY during development.
+# Do not enter a real emergency-service number.
+emergencyTestNumber = "your_emergency_test_number"
